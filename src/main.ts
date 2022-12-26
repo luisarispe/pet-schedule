3dea761eab2b03cf2bcdee4f2fef960516f0ea59
luisarispe <luis.arispe1991@gmail.com>
2022-12-26 17:44:30 -0300

    probando con cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
