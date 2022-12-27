16182cb65c42ab7860b952050677827ab6901c71
luisarispe <luis.arispe1991@gmail.com>
2022-12-27 16:06:11 -0300

    eliminando el maximo de memoria por petición

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
 
-  app.use(bodyParser.json({ limit: '5mb' }));
-  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
